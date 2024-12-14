18758117cd8d2142403877487fb130ba17fbdb3a
ashwinn-si <ashwin23cse@gmail.com>
2024-12-14 19:27:27 +0530

    Components all over

--- src/Components/ToggleHeader.jsx
+++ src/Components/ToggleHeader.jsx
@@ -1,20 +1,23 @@
 import "../Styles/CommonStyle.css"
-import {useState} from "react";
-
+import "../Styles/ToggleHeaderStyle.css"
 function ToggleHeader() {
 
 
     return (
-        <div className="container-fluid vh-7-5 primary-bg-color p-0 text-center mt-1">
-            <div
-                className="row h-100 w-100 d-flex justify-content-center align-items-center p-0 m-0 primary-text-color">
-                <div className="col-6 primary-text-color">
-                    CPGA
+        <div className="container-fluid vh-7-5 primary-bg-color p-0 text-center mt-1 mydict">
+
+                <div>
+                    <label>
+                        <input type="radio" name="radio"/>
+                        <span>CGPA</span>
+                    </label>
+                    <label>
+                        <input type="radio" name="radio"/>
+                        <span>GPA</span>
+                    </label>
+
                 </div>
-                <div className="col-6 primary-text-color">
-                    GPA
-                </div>
-            </div>
+
         </div>
     );
 }
