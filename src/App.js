34b4be2da270cf0a51ccf5b40573337a0983fecf
ashwinn-si <ashwin23cse@gmail.com>
2024-12-17 20:12:30 +0530

    Project Over....

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
               window.innerWidth > 768 ? <CustomCursor/> : null
             }
               <ToggleHeader />
+              <p className="primary-text-color fw-light text-center m-3 fs-s">Developed : <span className="hilight-text-color fs-s credit "  onClick={() => window.open("https://www.linkedin.com/in/ashwinsi/", "_blank")}>Ashwin SI </span></p>
           </MainFrame>
   );
 }
